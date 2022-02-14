52ad1f9a279875ea961b721b7249a0841ec18bdb
sanghyeonsh <ksh3967@naver.com>
2022-02-14 19:32:02 +0900

    first commit

---
+++ Algorithm/src/BaekJoon/Main_14503_김상현.java
@@ -0,0 +1,45 @@
+package BaekJoon;
+//다시
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_14503_김상현 {
+
+	static int[][] map;
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		int r = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		int d = Integer.parseInt(st.nextToken());
+		map = new int[N][M];
+		for(int i=0;i<N;i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0;j<M;j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		
+		
+//		for(int i=0;i<N;i++) {
+//			for(int j=0;j<M;j++) {
+//				System.out.print(map[i][j]+" ");
+//			}
+//			System.out.println();
+//		}
+		
+	}
+	
+	static void clean(int r, int c, int d) {
+		
+	}
+
+}
